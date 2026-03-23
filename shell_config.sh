057f832bb89ec6db1fd8907b82198e189f19c6d2
Leanne Bruton <leanne@bruton.local>
2026-03-23 09:50:28 -0700

    Rework nvm config

--- shell_config.sh
+++ shell_config.sh
@@ -1,7 +1,11 @@
 source ~/dotfiles/git_config.sh
 source ~/dotfiles/kubectl_config.sh
-source ~/dotfiles/nvm_config.sh
 source ~/dotfiles/rails_config.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source ~/dotfiles/nvm_config.sh
 
 if [ -f ~/dotfiles/local_aliases.sh ]; then
     . ~/dotfiles/local_aliases.sh
